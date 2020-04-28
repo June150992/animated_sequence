6e508b3561824fba69f4ff55d235ac89734bf5f4
unknown <julie.levin@live.dk>
2020-04-28 16:07:32 +0200

    step 3

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function showScreen1() {
 
     document.querySelector("#txt_1").classList.add("pulse");
 
-    document.querySelector("#next").addEventListener("click", hideScreen1);
+    document.querySelector("#next_1").addEventListener("click", hideScreen1);
 }
 
 function hideScreen1() {
@@ -26,7 +26,7 @@ function hideScreen1() {
     document.querySelector("#screen_1").classList.remove("show");
     document.querySelector("#screen_1").classList.add("hide");
 
-    document.querySelector("#screen_2").addEventListener("animationend", showScreen2);
+    document.querySelector("#screen_1").addEventListener("animationend", showScreen2);
 }
 
 function showScreen2() {
@@ -36,7 +36,7 @@ function showScreen2() {
     document.querySelector("#screen_2").classList.remove("hide");
 
     document.querySelector("#txt_2").classList.add("pulse");
-    document.querySelector("#next").addEventListener("click", hideScreen2);
+    document.querySelector("#next_2").addEventListener("click", hideScreen2);
 
 }
 function hideScreen2() {
@@ -47,5 +47,25 @@ function hideScreen2() {
     document.querySelector("#screen_2").classList.remove("show");
     document.querySelector("#screen_2").classList.add("hide");
 
-    document.querySelector("#screen_3").addEventListener("animationend", showScreen3);
+    document.querySelector("#screen_2").addEventListener("animationend", showScreen3);
+}
+
+function showScreen3() {
+    console.log("showScreen3");
+
+    document.querySelector("#screen_3").classList.add("show");
+    document.querySelector("#screen_3").classList.remove("hide");
+
+    document.querySelector("#txt_3").classList.add("pulse");
+    document.querySelector("#next_3").addEventListener("click", hideScreen3);
+}
+function hideScreen3() {
+
+    console.log("hideScreen3");
+
+    document.querySelector("#screen_3").classList.add("fade_out");
+    document.querySelector("#screen_3").classList.remove("show");
+    document.querySelector("#screen_3").classList.add("hide");
+
+    document.querySelector("#screen_3").addEventListener("animationend", showScreen4);
 }
